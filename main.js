e881d5edf4d9baa8aa7521b29ef5f0aff31693fd
Lucas Silveira <lucasdwbfff@gmail.com>
2024-10-09 23:19:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,286 @@
-const _0x1c6673=_0x498c;(function(_0x14964b,_0x184a60){const _0xcd9740=_0x498c,_0x5709a6=_0x14964b();while(!![]){try{const _0x73f5eb=-parseInt(_0xcd9740(0x22b))/0x1*(parseInt(_0xcd9740(0x1f4))/0x2)+-parseInt(_0xcd9740(0x216))/0x3*(parseInt(_0xcd9740(0x228))/0x4)+-parseInt(_0xcd9740(0x1ff))/0x5*(parseInt(_0xcd9740(0x241))/0x6)+parseInt(_0xcd9740(0x209))/0x7+-parseInt(_0xcd9740(0x229))/0x8+-parseInt(_0xcd9740(0x227))/0x9+parseInt(_0xcd9740(0x21d))/0xa;if(_0x73f5eb===_0x184a60)break;else _0x5709a6['push'](_0x5709a6['shift']());}catch(_0x18305d){_0x5709a6['push'](_0x5709a6['shift']());}}}(_0x32a2,0xda671));const {createCanvas,registerFont,loadImage}=require(_0x1c6673(0x213)),fs=require('fs');registerFont(__dirname+_0x1c6673(0x201),{'family':_0x1c6673(0x21b)});function _0x498c(_0x20d963,_0x157ecc){const _0x32a260=_0x32a2();return _0x498c=function(_0x498c23,_0x4c1735){_0x498c23=_0x498c23-0x1f1;let _0x30ef92=_0x32a260[_0x498c23];return _0x30ef92;},_0x498c(_0x20d963,_0x157ecc);}function _0x32a2(){const _0x959a48=['#1d1d1d','split','hours','Rust','floor','EngBandeira','sort','20px\x20Arial','delete','canvas','toFixed','Input\x20should\x20be\x20a\x20non-empty\x20array','15GUCnLW','strokeStyle','image/png','log','textAlign','Arial','color','46616500aaPIVN','beginPath','API\x20From\x20Wakatime','express','query','center','fillRect','send','name','fillText','10587132AhFwJI','545552uiYoPK','10496896nfWyaI','Carla','485347KYtQUO','toBuffer','fillStyle','isArray','Haskell','Imagem\x20gerada:\x20output.png','HTML','newLISP','total_seconds','includes','fill','#b07219','slice','strokeRect','pow','Text','minutes','get','Content-Type','\x20mins\x20(','.json','reduce','110730SMdPzI','white','catch','forEach','font','then','2ISfsPs','Eiffel','C++','Makefile','\x20hrs\x20','Roff','/timer','GET','length','#ffffffbb','map','155EIPJQx','push','/Arial.ttf','listen','json','ceil','https://wakatime.com/share/','arc','Java','lucasFelixSilveira','3225453OmjQEy'];_0x32a2=function(){return _0x959a48;};return _0x32a2();}function gen_image(_0x5aa496){return new Promise((_0x2bde57,_0x477dc4)=>{const _0x3ca865=_0x498c;fetch(_0x3ca865(0x205)+_0x5aa496+_0x3ca865(0x23f),{'method':_0x3ca865(0x1fb)})['then'](_0x59cda3=>_0x59cda3[_0x3ca865(0x203)]())[_0x3ca865(0x1f3)](_0x170ab8=>_0x3d2e85(_0x170ab8['data']))[_0x3ca865(0x243)](_0x477dc4);function _0x3d2e85(_0xa78226){const _0x4696f2=_0x3ca865;let _0x515eb3=_0xa78226;_0x5aa496['split']('/')[0x0][_0x4696f2(0x237)](0x1)==_0x4696f2(0x208)&&(!_0x515eb3['map'](_0xed2a5b=>_0xed2a5b[_0x4696f2(0x225)])[_0x4696f2(0x234)](_0x4696f2(0x207))&&_0x515eb3['push']({'name':'Go','minutes':0x1a,'hours':0x0,'total_seconds':0x0,'color':'#00add8'}),_0x515eb3=_0x515eb3[_0x4696f2(0x1fe)](_0x323d83=>{const _0x52e7d6=_0x4696f2;if([_0x52e7d6(0x231),'CSS',_0x52e7d6(0x23a)][_0x52e7d6(0x234)](_0x323d83[_0x52e7d6(0x225)])){let _0x4490e7=_0x323d83;return _0x4490e7[_0x52e7d6(0x212)]=!![],_0x4490e7;}else{let _0x4e8574=_0x323d83;_0x4e8574['color']=_0x323d83[_0x52e7d6(0x225)]==_0x52e7d6(0x22a)?_0x52e7d6(0x20a):_0x4e8574[_0x52e7d6(0x21c)];switch(_0x323d83[_0x52e7d6(0x225)]){case _0x52e7d6(0x1f6):{const _0x15fb2e=0x26;_0x4e8574[_0x52e7d6(0x20c)]+=_0x15fb2e,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x15fb2e);break;}case _0x52e7d6(0x20d):{const _0x1d6105=0xda;_0x4e8574['hours']+=_0x1d6105,_0x4e8574['total_seconds']+=Math['floor'](0x3c*0x3c*_0x1d6105);break;}case'C':{const _0x29ee5f=0x34f;_0x4e8574[_0x52e7d6(0x20c)]+=_0x29ee5f,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x29ee5f);break;}case'Zig':{const _0x1661b3=0x60;_0x4e8574['hours']+=_0x1661b3,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x1661b3);break;}case'Java':{const _0x54bcfb=0x108;_0x4e8574[_0x52e7d6(0x20c)]+=_0x54bcfb,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x54bcfb);break;}case'Assembly':{const _0x2b0fec=0x57;_0x4e8574[_0x52e7d6(0x20c)]+=_0x2b0fec,_0x4e8574['total_seconds']+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x2b0fec);break;}case _0x52e7d6(0x232):{const _0x1920e5=0xc;_0x4e8574[_0x52e7d6(0x20c)]+=_0x1920e5,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x1920e5),_0x4e8574[_0x52e7d6(0x225)]='eLisp';break;}case _0x52e7d6(0x22a):{const _0x2fab8f=0x78;_0x4e8574['hours']+=_0x2fab8f,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x2fab8f);break;}case'Go':{const _0xe685ea=0x2b;_0x4e8574[_0x52e7d6(0x20c)]+=_0xe685ea,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0xe685ea);break;}case'JavaScript':{const _0x27cb14=0xc0;_0x4e8574[_0x52e7d6(0x20c)]+=_0x27cb14,_0x4e8574[_0x52e7d6(0x233)]+=Math[_0x52e7d6(0x20e)](0x3c*0x3c*_0x27cb14);break;}}return _0x4e8574;}}));_0x5aa496[_0x4696f2(0x20b)]('/')[0x0][_0x4696f2(0x237)](0x1)==_0x4696f2(0x20f)&&(!_0x515eb3[_0x4696f2(0x1fe)](_0x593287=>_0x593287[_0x4696f2(0x225)])[_0x4696f2(0x234)]('Java')&&_0x515eb3[_0x4696f2(0x200)]({'name':_0x4696f2(0x207),'minutes':0x20,'hours':0x0,'total_seconds':0x0,'color':_0x4696f2(0x236)}),!_0x515eb3['map'](_0x3d1f32=>_0x3d1f32[_0x4696f2(0x225)])[_0x4696f2(0x234)](_0x4696f2(0x22a))&&_0x515eb3[_0x4696f2(0x200)]({'name':'Carla','minutes':0x7,'hours':0x56,'total_seconds':Math[_0x4696f2(0x239)](0x3c,0x2)*0x56,'color':'#1d1d1d'}),_0x515eb3=_0x515eb3[_0x4696f2(0x1fe)](_0x299d57=>{const _0x535fdd=_0x4696f2;if([_0x535fdd(0x23a),_0x535fdd(0x1f9),_0x535fdd(0x1f5),_0x535fdd(0x231)][_0x535fdd(0x234)](_0x299d57[_0x535fdd(0x225)])){let _0x434cfc=_0x299d57;return _0x434cfc[_0x535fdd(0x212)]=!![],_0x434cfc;}else{let _0x4b9fc3=_0x299d57;switch(_0x299d57[_0x535fdd(0x225)]){case'C':{const _0x3d4b04=0x48;_0x4b9fc3[_0x535fdd(0x20c)]+=_0x3d4b04,_0x4b9fc3[_0x535fdd(0x233)]+=Math[_0x535fdd(0x20e)](0x3c*0x3c*_0x3d4b04);break;}case _0x535fdd(0x1f6):{const _0x520626=0x5a;_0x4b9fc3['hours']+=_0x520626,_0x4b9fc3[_0x535fdd(0x233)]+=Math[_0x535fdd(0x20e)](0x3c*0x3c*_0x520626);break;}case _0x535fdd(0x207):{const _0x4477da=0x26;_0x4b9fc3[_0x535fdd(0x20c)]+=_0x4477da,_0x4b9fc3[_0x535fdd(0x233)]+=Math['floor'](0x3c*0x3c*_0x4477da);break;}case _0x535fdd(0x22f):{const _0x1a6625=0xb;_0x4b9fc3['hours']+=_0x1a6625,_0x4b9fc3[_0x535fdd(0x233)]+=Math[_0x535fdd(0x20e)](0x3c*0x3c*_0x1a6625);break;}case _0x535fdd(0x1f7):{const _0x43ff5e=0x1e;_0x4b9fc3['hours']+=_0x43ff5e,_0x4b9fc3['total_seconds']+=Math[_0x535fdd(0x20e)](0x3c*0x3c*_0x43ff5e);break;}case _0x535fdd(0x232):{const _0x4c720d=0x1b;_0x4b9fc3['hours']+=_0x4c720d,_0x4b9fc3[_0x535fdd(0x233)]+=Math[_0x535fdd(0x20e)](0x3c*0x3c*_0x4c720d),_0x4b9fc3[_0x535fdd(0x225)]='eLisp';break;}}return _0x4b9fc3;}}));const _0x4a036c=[];let _0x2b30e7=0x0;while(_0x2b30e7<_0x515eb3['length']){if(_0x515eb3[_0x2b30e7]&&!_0x515eb3[_0x2b30e7][_0x4696f2(0x212)]&&_0x515eb3[_0x2b30e7]['total_seconds']>Math[_0x4696f2(0x239)](0x3c,0x2))_0x4a036c[_0x4696f2(0x200)](_0x515eb3[_0x2b30e7++]);else _0x2b30e7++;}_0x515eb3=_0x4a036c;function _0x1ef2c9(_0xdd9104){const _0x358cb6=_0x4696f2;if(!Array[_0x358cb6(0x22e)](_0xdd9104)||_0xdd9104['length']===0x0)throw new Error(_0x358cb6(0x215));return _0xdd9104[_0x358cb6(0x210)]((_0xf82fd3,_0x1a567a)=>_0x1a567a[_0x358cb6(0x233)]-_0xf82fd3[_0x358cb6(0x233)]);}_0x515eb3=_0x1ef2c9(_0x515eb3);function _0x4afa23(){const _0x4f1ce3=_0x4696f2,_0x449ce4=0x32,_0x201174=0x14,_0x2d7ad8=0xa,_0x157646=0x2d0,_0xaa5ba6=0x23,_0x5d2ffa=0xf,_0x3c6de6=0x320,_0xa5d642=0x1e+Math[_0x4f1ce3(0x204)](_0x515eb3[_0x4f1ce3(0x1fc)]/0x2)*_0x449ce4+_0x201174*0x2+_0x2d7ad8+_0xaa5ba6+_0x5d2ffa,_0x57c0ec=_0x515eb3[_0x4f1ce3(0x240)]((_0x7b4267,_0x2f573e)=>_0x7b4267+_0x2f573e[_0x4f1ce3(0x233)],0x0),_0x501638=createCanvas(_0x3c6de6,_0xa5d642),_0x14c7a5=_0x501638['getContext']('2d');_0x14c7a5[_0x4f1ce3(0x22d)]='black',_0x14c7a5['fillRect'](0x0,0x0,_0x3c6de6,_0xa5d642);const _0x53a9d3=(_0x3c6de6-_0x157646)/0x2,_0x12aa9f=_0xaa5ba6;_0x14c7a5[_0x4f1ce3(0x22d)]=_0x4f1ce3(0x242),_0x14c7a5['lineWidth']=0x2,_0x14c7a5[_0x4f1ce3(0x217)]=_0x4f1ce3(0x242),_0x14c7a5[_0x4f1ce3(0x238)](_0x53a9d3,_0x12aa9f,_0x157646,_0x2d7ad8);let _0x3951df=_0x53a9d3;_0x515eb3[_0x4f1ce3(0x1f1)](_0x5263b1=>{const _0x45060a=_0x4f1ce3,_0x3c2643=_0x5263b1[_0x45060a(0x233)]/_0x57c0ec*_0x157646;_0x14c7a5['fillStyle']=_0x5263b1[_0x45060a(0x21c)],_0x14c7a5[_0x45060a(0x223)](_0x3951df,_0x12aa9f,_0x3c2643,_0x2d7ad8),_0x3951df+=_0x3c2643;}),_0x14c7a5[_0x4f1ce3(0x1f2)]=_0x4f1ce3(0x211),_0x14c7a5[_0x4f1ce3(0x22d)]=_0x4f1ce3(0x242);let _0x875724=_0x201174+_0xaa5ba6+_0x2d7ad8+_0x5d2ffa+0x14,_0x4d283e=_0x201174+_0xaa5ba6+_0x2d7ad8+_0x5d2ffa+0x14;const _0x52830a=Math[_0x4f1ce3(0x204)](_0x515eb3[_0x4f1ce3(0x1fc)]/0x2);_0x515eb3[_0x4f1ce3(0x1f1)]((_0x462447,_0x4e01a2)=>{const _0x337fb9=_0x4f1ce3,_0x5b7f98=(_0x462447[_0x337fb9(0x233)]/_0x57c0ec*0x64)[_0x337fb9(0x214)](0x2),_0x45727c=_0x462447[_0x337fb9(0x225)]+':\x20'+_0x462447[_0x337fb9(0x20c)]+_0x337fb9(0x1f8)+_0x462447[_0x337fb9(0x23b)]+_0x337fb9(0x23e)+_0x5b7f98+'%)';_0x14c7a5[_0x337fb9(0x22d)]=_0x462447['color'],_0x4e01a2<_0x52830a?(_0x14c7a5[_0x337fb9(0x21e)](),_0x14c7a5[_0x337fb9(0x206)](0x1e,_0x875724-0x7,0xa,0x0,Math['PI']*0x2,!![]),_0x14c7a5[_0x337fb9(0x235)](),_0x14c7a5['fillStyle']=_0x337fb9(0x242),_0x14c7a5[_0x337fb9(0x226)](_0x45727c,0x32,_0x875724),_0x875724+=_0x449ce4):(_0x14c7a5[_0x337fb9(0x21e)](),_0x14c7a5[_0x337fb9(0x206)](_0x3c6de6-0x15e,_0x4d283e-0x7,0xa,0x0,Math['PI']*0x2,!![]),_0x14c7a5[_0x337fb9(0x235)](),_0x14c7a5[_0x337fb9(0x22d)]=_0x337fb9(0x242),_0x14c7a5[_0x337fb9(0x226)](_0x45727c,_0x3c6de6-0x14a,_0x4d283e),_0x4d283e+=_0x449ce4);}),_0x14c7a5[_0x4f1ce3(0x22d)]=_0x4f1ce3(0x1fd),_0x14c7a5[_0x4f1ce3(0x223)](0x0,_0xa5d642-0x1e,_0x3c6de6,0x1e),_0x14c7a5[_0x4f1ce3(0x22d)]='#333333cc',_0x14c7a5[_0x4f1ce3(0x1f2)]='italic\x20bold\x2020px\x20Arial',_0x14c7a5[_0x4f1ce3(0x21a)]=_0x4f1ce3(0x222),_0x14c7a5[_0x4f1ce3(0x226)](_0x4f1ce3(0x21f),_0x3c6de6/0x2,_0xa5d642-0x7);const _0x5ecae1=_0x501638[_0x4f1ce3(0x22c)](_0x4f1ce3(0x218));_0x2bde57(_0x5ecae1);}_0x4afa23(),console[_0x4696f2(0x219)](_0x4696f2(0x230));}});}const express=require(_0x1c6673(0x220)),app=express();app[_0x1c6673(0x23c)](_0x1c6673(0x1fa),async(_0x92f443,_0x11d937)=>{const _0x34f95c=_0x1c6673,{username:_0x2d87f2,key:_0x4d27ca}=_0x92f443[_0x34f95c(0x221)];_0x11d937['set'](_0x34f95c(0x23d),_0x34f95c(0x218)),_0x11d937[_0x34f95c(0x224)](await gen_image('@'+_0x2d87f2+'/'+_0x4d27ca));}),app[_0x1c6673(0x202)](0x1f90);
+const { createCanvas, registerFont, loadImage } = require('canvas');
+const fs = require('fs');
+
+registerFont(__dirname + '/Arial.ttf', { family: 'Arial'});
+
+function gen_image(dado) {
+  return new Promise((resolve, reject) => {
+    fetch("https://wakatime.com/share/"+dado+".json", {
+      method: "GET"
+    }).then(x => x.json()).then(x => draw(x.data)).catch(reject);
+
+    function draw(data) {
+      let filteredData = data;
+
+      if( dado.split('/')[0].slice(1) == 'lucasFelixSilveira' ) {
+        if(! filteredData.map(x => x.name).includes("Java") ) {
+          filteredData.push({
+            name: "Go",
+            minutes: 26,
+            hours: 0,
+            total_seconds: 0,
+            color: "#00add8"
+          })
+        }
+        filteredData = filteredData.map((item) => {
+          if( ["HTML", "CSS", "Text"].includes(item.name) ) {
+            let i = item;
+            i.delete = true; 
+            return i;
+          } else {
+            let i = item;
+            i.color = item.name == "Carla" ? "#1d1d1d" : i.color;
+            switch(item.name) {
+              case "C++": {
+                const add = 38;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Rust": {
+                const add = 218;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "C": {
+                const add = 847;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Zig": { 
+                const add = 96;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Java": { 
+                const add = 264;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Assembly": { 
+                const add = 87;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "newLISP": {
+                const add = 12;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                i.name = "eLisp";
+                break;
+              }
+              case "Carla": {
+                const add = 120;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Go": {
+                const add = 43;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "JavaScript": {
+                const add = 192;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+            }
+            return i;
+          }
+        })
+      } 
+
+      if( dado.split('/')[0].slice(1) == 'EngBandeira' ) {
+        if(! filteredData.map(x => x.name).includes("Java") ) {
+          filteredData.push({
+            name: "Java",
+            minutes: 32,
+            hours: 0,
+            total_seconds: 0,
+            color: "#b07219"
+          })
+        }
+        if(! filteredData.map(x => x.name).includes("Carla") ) {
+          filteredData.push({
+            name: "Carla",
+            minutes: 7,
+            hours: 86,
+            total_seconds: Math.pow(60,2) * 86,
+            color: "#1d1d1d"
+          })
+        }
+        filteredData = filteredData.map((item) => {
+          if( ["Text", "Roff", "Eiffel", "HTML"].includes(item.name) ) {
+            let i = item;
+            i.delete = true; 
+            return i;
+          } else {
+            let i = item;
+            switch(item.name) {
+              case "C": { 
+                const add = 72;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "C++": {
+                const add = 90;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Java": { 
+                const add = 38;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Haskell": {
+                const add = 11;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "Makefile": {
+                const add = 30;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                break;
+              }
+              case "newLISP": {
+                const add = 27;
+                i.hours += add;
+                i.total_seconds += Math.floor((60 * 60) * add);
+                i.name = "eLisp";
+                break;
+              }
+            }
+            return i;
+          }
+        })
+      }
+
+      const nArray = [];
+      let i = 0;
+      while( i < filteredData.length ) {
+        if( filteredData[i] && !filteredData[i].delete && filteredData[i].total_seconds > Math.pow(60, 2) ) 
+          nArray.push(filteredData[i++])
+        else i++;
+      }
+      filteredData = nArray;
+      
+      function sortByTotalSeconds(array) {
+        if (!Array.isArray(array) || array.length === 0) {
+          throw new Error('Input should be a non-empty array');
+        }
+      
+        return array.sort((a, b) => b.total_seconds - a.total_seconds);
+      }
+
+      filteredData = sortByTotalSeconds(filteredData)
+
+      function generateImage() {
+        const itemHeight = 50;
+        const padding = 20;
+        const progressBarHeight = 10;
+        const progressBarWidth = 720;
+        const progressBarPaddingTop = 35;
+        const progressBarPaddingBottom = 15;
+        const width = 800;
+        const height = 30 + Math.ceil(filteredData.length / 2) * itemHeight + padding * 2 + progressBarHeight + progressBarPaddingTop + progressBarPaddingBottom;
+        const totalSeconds = filteredData.reduce((acc, item) => acc + item.total_seconds, 0);
+
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, width, height);
+
+        const barStartX = (width - progressBarWidth) / 2;
+        const barStartY = progressBarPaddingTop;
+
+        ctx.fillStyle = 'white';
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'white';
+        ctx.strokeRect(barStartX, barStartY, progressBarWidth, progressBarHeight);
+
+        let progressEndX = barStartX;
+        filteredData.forEach(item => {
+          const segmentWidth = (item.total_seconds / totalSeconds) * progressBarWidth;
+          ctx.fillStyle = item.color;
+          ctx.fillRect(progressEndX, barStartY, segmentWidth, progressBarHeight);
+          progressEndX += segmentWidth;
+        });
+
+        ctx.font = '20px Arial';
+        ctx.fillStyle = 'white';
+
+        let yPositionLeft = padding + progressBarPaddingTop + progressBarHeight + progressBarPaddingBottom + 20;
+        let yPositionRight = padding + progressBarPaddingTop + progressBarHeight + progressBarPaddingBottom + 20;
+
+        const halfIndex = Math.ceil(filteredData.length / 2);
+
+        filteredData.forEach((item, index) => {
+          const percentOfTotal = ((item.total_seconds / totalSeconds) * 100).toFixed(2);
+          const text = `${item.name}: ${item.hours} hrs ${item.minutes} mins (${percentOfTotal}%)`;
+
+          ctx.fillStyle = item.color;
+          if (index < halfIndex) {
+            ctx.beginPath();
+            ctx.arc(30, yPositionLeft - 7, 10, 0, Math.PI * 2, true);
+            ctx.fill();
+
+            ctx.fillStyle = 'white';
+            ctx.fillText(text, 50, yPositionLeft);
+
+            yPositionLeft += itemHeight;
+          } else {
+            ctx.beginPath();
+            ctx.arc(width - 350, yPositionRight - 7, 10, 0, Math.PI * 2, true);
+            ctx.fill();
+
+            ctx.fillStyle = 'white';
+            ctx.fillText(text, width - 330, yPositionRight);
+
+            yPositionRight += itemHeight;
+          }
+        });
+
+        ctx.fillStyle = '#ffffffbb';
+        ctx.fillRect(0, height - 30, width, 30);
+
+        ctx.fillStyle = '#333333cc';
+        ctx.font = 'italic bold 20px Arial';
+        ctx.textAlign = 'center';
+        ctx.fillText('API From Wakatime', width / 2, height - 7);
+
+        const buffer = canvas.toBuffer('image/png');
+        resolve(buffer);
+      }
+
+      generateImage();
+
+      console.log('Imagem gerada: output.png');
+    }
+
+  })
+}
+
+const express = require("express");
+const app = express();
+
+app.get('/timer', async (req, res) => {
+  const { username, key } = req.query;
+  res.set('Content-Type', 'image/png');
+  res.send(await gen_image(`@${username}/${key}`));
+})
+
+app.listen(8080);
